dd110f65e24fd86685d4ef3b9f86c866d3256662
Destinyr4zr <pristine.kirill@gmail.com>
2019-12-15 00:31:24 +0300

    restruct

--- Lab45/Lab4_with_Jetty/src/main/resources/Lab4_frontend/user.js
+++
@@ -1,7 +0,0 @@
-class pencil{
-    constructor(date, col, length){
-        this.date = date;
-        this.col = col;
-        this.length = length;
-    }
-}
--- Lab45/Lab4_with_Jetty/src/main/ru/mirea/KSP/Lab4_backend/Result.java
+++
@@ -1,5 +0,0 @@
-package main.ru.mirea.KSP.Lab4_backend;
-
-public interface Result {
-    StringBuilder getResult();
-}
